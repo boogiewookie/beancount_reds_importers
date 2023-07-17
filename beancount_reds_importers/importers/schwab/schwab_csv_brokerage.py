73cc3f5c78dcdbfc8a9289db9040c7f0530e5b68
Kevet Duncombe <kevetduncombe@gmail.com>
2023-07-17 16:41:09 -0500

    Adapt schwab_csv_brokerage.py to dunc style input

--- beancount_reds_importers/importers/schwab/schwab_csv_brokerage.py
+++ beancount_reds_importers/importers/schwab/schwab_csv_brokerage.py
@@ -1,5 +1,6 @@
 """ Schwab Brokerage .csv importer."""
 
+import re
 from beancount_reds_importers.libreader import csvreader
 from beancount_reds_importers.libtransactionbuilder import investments
 
@@ -8,6 +9,7 @@ class Importer(csvreader.Importer, investments.Importer):
     IMPORTER_NAME = 'Schwab Brokerage CSV'
 
     def custom_init(self):
+        self.year, self.month = self.config.get("ym", "").split('-', 2)
         self.max_rounding_error = 0.04
         self.filename_pattern_def = '.*_Transactions_'
         self.header_identifier = rf'"Transactions  for account {self.config.get("account_number", "")}.*'
@@ -55,9 +57,36 @@ class Importer(csvreader.Importer, investments.Importer):
             'Stock Split':                  'transfer',
             'Spin-off':                     'transfer',  # TODO: not handled correctly
             'Cash In Lieu':                 'transfer',  # TODO: not handled correctly
+            'CD Deposit Adj':               'transfer',
+            'CD Deposit Funds':             'transfer',
             }
         self.skip_transaction_types = ['Journal']
         self.skip_head_rows = 1
+        self.acctmap = {
+                "...164": "AKSB 0000440000810164",
+                "Kevet Inherited IRA ...020":   "AKII 2583-5020",
+                "Kevet Inherited IRA XXXX-5020":"AKII 2583-5020",
+                "Kevet PledgedAsset ...972":    "AKPA 3058-9972",
+                "Kevet PledgedAsset XXXX-9972": "AKPA 3058-9972",
+                "Kevet RothIRA ...218":         "AKRI 9048-1218",
+                "Kevet RothIRA XXXX-1218":      "AKRI 9048-1218",
+                "Kevet Schwab One ...772":      "AKSO 2993-9772",
+                "Kevet Schwab One XXXX-9772":   "AKSO 2993-9772",
+                "Kevet SchwabIRA ...775":       "AKSI 2993-9775",
+                "Kevet SchwabIRA XXXX-9775":    "AKSI 2993-9775",
+                "Sandy IRA ...635":             "ASSI 2994-3635",
+                "Sandy-SchwabIRA ...635":       "ASSI 2994-3635",
+                "Sandy IRA XXXX-3635":          "ASSI 2994-3635",
+                "Sandy Inh IRA ...348":         "ASII 6884-1348",
+                "Sandy-Inh.IRA ...348":         "ASII 6884-1348",
+                "Sandy Inh IRA XXXX-1348":      "ASII 6884-1348",
+                "Sandy-InheritedIRA ...348":    "ASII 6884-1348",
+                "Sandy Schwab One ...874":      "ASSO 8026-8874",
+                "Sandy Schwab One XXXX-8874":   "ASSO 8026-8874",
+                "Sandy-SchwabTinman ...420":    "ASST 9407-3420",
+                "Sandy-SchwabTaxable ...420":   "ASST 9407-3420",
+                "Sandy-SchwabTaxable XXXX-3420":"ASST 9407-3420",
+                }
 
     def prepare_table(self, rdr):
         if '' in rdr.fieldnames():
@@ -67,7 +96,17 @@ class Importer(csvreader.Importer, investments.Importer):
             """'11/16/2018 as of 11/15/2018' --> '11/16/2018'"""
             return d.split(' ', 1)[0]
 
+        
         rdr = rdr.convert('Date', cleanup_date)
+        rdr = rdr.select('Date', lambda v: (v.startswith(self.month) and v.endswith(self.year)))
         rdr = rdr.addfield('tradeDate', lambda x: x['Date'])
         rdr = rdr.addfield('total', lambda x: x['Amount'])
         return rdr
+
+    def deep_identify(self, file):
+        head = file.head()
+        want = self.config.get("account_number", "")
+        for raw, acct in self.acctmap.items():
+            if acct == want and re.match(rf'"Transactions  for account {raw}.*', head):
+                return True
+        return False
