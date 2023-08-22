e7b5a80eb3586b6246059b0ad0d42ca5d6a80f05
Kevet Duncombe <kevetduncombe@gmail.com>
2023-08-22 08:23:58 -0500

    Amex credit card .qfx files use negative values labled credit for charges, positive values labeled credit for payments received and refunds. That's what beancount expects. But for some insane reason, Amex savings .qfx files label money going out as a debit, money coming in as a credit, and use positive numbers for both! To get what beancount expects we need to flip the sign on values labled debit.

--- beancount_reds_importers/importers/amex/__init__.py
+++ beancount_reds_importers/importers/amex/__init__.py
@@ -1,4 +1,4 @@
-"""Amex credit card ofx importer for beancount."""
+"""Amex credit card and savings ofx importer for beancount."""
 
 from beancount_reds_importers.libreader import ofxreader
 from beancount_reds_importers.libtransactionbuilder import banking
@@ -11,12 +11,13 @@ class Importer(banking.Importer, ofxreader.Importer):
         if not self.custom_init_run:
             self.max_rounding_error = 0.04
             self.filename_pattern_def = '.*amex'
+            self.flip_debit_sign = self.config.get("flip_debit_sign", False)
             self.custom_init_run = True
 
     def get_transactions(self):
         """Make the amount match the type."""
         for ot in self.ofx_account.statement.transactions:
-            if ot.type == 'debit':
+            if ot.type == 'debit' and self.flip_debit_sign:
                 ot.amount = -ot.amount
             yield ot
 
